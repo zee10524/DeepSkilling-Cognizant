fed3b23c1c621e2cf3361557f5da0a5370bac667
Mohammad Zeeshan Ahmad <zeeshan10524@gmail.com>
2026-07-01 15:51:43 +0530

    week2 completed

---
+++ Week2_SpringDataJPA_Hibernate/Exercise-2 Employee Management System - Creating Entities/Department.java
@@ -0,0 +1,28 @@
+package com.example.employeemanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "departments")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Employee> employees;
+
+    //Additional constructors, methods, etc., may be added here as per need
+}
---
+++ Week2_SpringDataJPA_Hibernate/Exercise-2 Employee Management System - Creating Entities/Employee.java
@@ -0,0 +1,29 @@
+package com.example.employeemanagementsystem.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "department_id")
+    private Department department;
+
+    //Additional constructors, methods, etc., may be added here as per need
+}
